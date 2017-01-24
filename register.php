ee01495bd369a325b5eacd0bb0f8d5219fb21b6c
alberttui <albert.tui@benilde.edu.ph>
2017-01-24 23:22:36 +0800

    Finalize Exercise 3
    Final

--- register.php
+++ register.php
@@ -1,85 +1,76 @@
 <?php
-
-$studentname =
-
+$studentNo = "";
+$lastName = "";
+$firstName = "";
+$emailAddress = "";
+$password = "";
+$birthDate = null;
 ?>
-
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Student Registration</title>
-		<link href="css/bootstrap.min.css" rel="stylesheet" />
+		<title>Welcome to the Student Registration Page of Berto's College</title>
+		<meta keyword="author" value="Albert Tui"/>
+		<meta keyword="description" value="WEBDEVT Exercise #3" />
+		<meta charset="utf-8" />
+		<link href="css/bootstrap.min.csss" type ="text/css" rel="stylesheet"/>
+		<link href="css/master.css" type ="text/css" rel ="stylesheet">
 	</head>
 	<body>
 		<div class="container">
 			<h1 class="text-center">Student Registration</h1>
-			<div class="col-lg-offset-3 col-lg-6">
 				<div class="well">
-					<form method="GET" action="welcome.php"
-						class="form-horizontal">
-						<div class="form-group">
-							<label class="control-label col-lg-4">
-								Student ID
-							</label>
-							<div class="col-lg-8">
-								<input type="text" name="sid"
-									class="form-control" required />
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-lg-4">
-								Last Name
-							</label>
-							<div class="col-lg-8">
-								<input type="text" name="ln"
-									class="form-control" required />
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-lg-4">
-								First Name
-							</label>
-							<div class="col-lg-8">
-								<input type="text" name="fn"
-									class="form-control" required />
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-lg-4">
-								Email Address
-							</label>
-							<div class="col-lg-8">
-								<input type="email" name="email"
-									class="form-control" required />
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-lg-4">
-								Password
-							</label>
-							<div class="col-lg-8">
-								<input type="password" name="pwd"
-									class="form-control" required />
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-lg-4">
-								Birthdate
-							</label>
-							<div class="col-lg-8">
-								<input type="date" name="bday"
-									class="form-control" required />
-							</div>
-						</div>
-						<div class="form-group">
-							<div class="control-label col-lg-4">
-								<button name="register"
-								class= "btn btn-success">
-								Register
-							</button>
-							</div>
-                        <div>
-					</form>
+					<form method="POST" action="welcome.php" class="form-horizontal">
+                     <br>
+					 <br>
+					 <div class="form-input">
+                     <input type="text" name="sid" placeholder="Enter Student ID #" class="form-control" required>
+                     </div>
+                     <br>
+
+                     <div class="form-input">
+                     <input type="text" name="ln" placeholder="Enter Last Name" class="form-control" required>
+                     </div>
+                     <br>
+
+                     <div class="form-input">
+                     <input type="text" name="fn" placeholder="Enter First Name" class="form-control" required>
+                     </div>
+                     <br>
+
+                     <div class="form-input">
+                     <input type="text" name="email" placeholder="Enter Email Address" class="form-control" required>
+                     </div>
+                     <br>
+
+                     <div class="form-input">
+                     <input type="password" name="pwd" placeholder="Enter Password" class="form-control" required>
+                     </div>
+					 <br>
+
+					 <div class="form-input">
+                     <input type="date" name="bday" placeholder="Enter Birthdate dd/mm/yyyy" class="form-control" required>
+                     </div>					 
+                     <br>
+					 <br>
+					
+					 <div class="form-group">
+					 <button name="register"
+					 class= "btn btn-success">
+					 Register
+					 </button>
+					 <br>
+					 <br>
+					 
+					 <footer class="foot">
+                     <hr>
+                     <h2>Berto's College</h2>
+					 </hr>
+                     </footer>
+					 
+					 </div>
+                     <div>
+					 </form>
 				</div>
 			</div>
 		</div>
---
+++ welcome.php
@@ -0,0 +1,52 @@
+<?php
+      if(isset($_POST['register']))
+      {
+        $studentNo = $_POST['sid'];
+        $lastName = $_POST['ln'];
+        $firstName = $_POST['fn'];
+        $emailAddress = $_POST['email'];
+        $password = $_POST['pwd'];
+        $birthDate = $_POST['bday'];
+      } 
+      else
+      {
+      	header('location: register.php');
+      }
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo "Welcome, ID " . $studentNo . "!"?></title>
+		<link href="css/bootstrap.min.csss" type ="text/css" rel="stylesheet"/>
+		<link href="css/style.css" type ="text/css" rel ="stylesheet">
+	</head>
+	<body>
+	<br>
+	<br>
+		<div class="prof-box">
+		<h1 class="text-center">Your Profile Page</h1>
+		<br>
+        <p>
+        <?php
+          echo "Welcome, " . $firstName . " " . $lastName . "!<br/>".
+          'Your ID Number is ' . $studentNo . '. <br/>
+		   Your Email is ' . $emailAddress . '. <br/>
+           Your password is ' . $password . '. <br/>
+           Your BirthDate is ' . $birthDate . '. <br/><br/><br/>
+           The time is ' . date('c');
+         ?>
+        </p>
+		<a href="register.php">
+        <button>Log Out</button>
+        </a>
+		</div>
+		</div>
+		<br>
+		<br>
+		<footer class="foot">
+        <hr>
+        <h2>Berto's College</h2>
+		</hr>
+        </footer>
+	</body>
+</html>
